a9be802932e00d39246f5b4d61de8694e20b0d5a
villsource <30929839+VillSource@users.noreply.github.com>
2025-12-11 07:02:21 +0700

    compleate work federation module

--- src/Application/BuzMag/src/app/app.routes.ts
+++ src/Application/BuzMag/src/app/app.routes.ts
@@ -1,3 +1,13 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadChildren: () =>
+            loadRemoteModule({
+                remoteName: 'user', 
+                exposedModule: './Component' 
+            }).then(m => m.App)
+    }
+];
--- src/Application/Villsource.BuzMag/Program.cs
+++ src/Application/Villsource.BuzMag/Program.cs
@@ -5,19 +5,31 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("dev", policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
 var app = builder.Build();
 
-// app.UseStaticFiles();
-// app.UseDefaultFiles();
-// app.MapFallbackToFile("index.html");
+app.UseStaticFiles();
+app.UseDefaultFiles();
+app.MapFallbackToFile("index.html");
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors("dev");
     app.MapOpenApi();
     app.MapScalarApiReference();
     app.MapGet("/", () => Results.Redirect("/scalar")).ExcludeFromDescription();
 }
 app.UseHttpsRedirection();
+
+app.MapGet("federation.manifest.json", (HttpRequest req) => Results.Json(new
+{
+    user = $"{req.Scheme}://{req.Host.Value}/Modules/Users/ui/remoteEntry.json",
+    test="from server"
+}));
 
 app.LoadModules(options =>
     options.ModulePath = "/home/pandora/Desktop/BuzMag/src/Modules/UserModule/Villsource.BuzMag.User/bin/Release/net10.0/Villsource.BuzMag.User//");
