bf1c26e94e503eebd3af95e0cc2f8b293096b185
VillSource <30929839+VillSource@users.noreply.github.com>
2026-08-02 01:53:51 +0700

    pick to startup module template

---
+++ src/VillsourceModules/Organization - Use To copy for new module/Villsource.FSH.Modules.Organization/Data/Configurations/OrganizationConfiguration.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Villsource.FSH.Modules.Organization.Data.Configurations;
+
+internal sealed class OrganizationConfiguration : IEntityTypeConfiguration<Domain.Organization>
+{
+    public void Configure(EntityTypeBuilder<Domain.Organization> builder)
+    {
+        builder.ToTable("Organization");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedNever();
+
+        builder.Ignore(x => x.DomainEvents);
+    }
+}
