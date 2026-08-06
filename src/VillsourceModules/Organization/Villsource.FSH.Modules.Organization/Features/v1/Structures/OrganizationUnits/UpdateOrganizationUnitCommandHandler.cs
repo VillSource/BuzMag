f795356d797e7cca25e69766a8f79a2ecd8189fb
VillSource <30929839+VillSource@users.noreply.github.com>
2026-08-06 12:12:30 +0700

    forgot to add handler

---
+++ src/VillsourceModules/Organization/Villsource.FSH.Modules.Organization/Features/v1/Structures/OrganizationUnits/UpdateOrganizationUnitCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using FSH.Framework.Core.Exceptions;
+using Mediator;
+using Microsoft.EntityFrameworkCore;
+using Villsource.FSH.Modules.Organization.Contracts.Dtos;
+using Villsource.FSH.Modules.Organization.Contracts.v1.Structures;
+using Villsource.FSH.Modules.Organization.Data;
+using Villsource.FSH.Modules.Organization.Domain;
+using Villsource.FSH.Modules.Organization.Mappers;
+
+namespace Villsource.FSH.Modules.Organization.Features.v1.Structures.OrganizationUnits;
+
+public sealed class UpdateOrganizationUnitCommandHandler(
+    OrganizationDbContext dbContext) : ICommandHandler<UpdateOrganizationUnitCommand, OrganizationUnitDto>
+{
+    public async ValueTask<OrganizationUnitDto> Handle(UpdateOrganizationUnitCommand command,
+        CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(command);
+
+        var ou = await dbContext.OrganizationUnits
+                     .FirstOrDefaultAsync(ou => ou.Id == command.OrganizationUnitId, cancellationToken)
+                     .ConfigureAwait(false) ??
+                 throw new NotFoundException($"Organization unit with id '{command.OrganizationUnitId}' not found.");
+
+        ou.Update(
+            code: command.Code,
+            name: command.Name,
+            description: command.Description
+        );
+
+        await dbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+
+        return ou.ToDto();
+    }
+}
