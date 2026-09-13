7b91865e6604b8db73c07debaf9d723881dd9237
VillSource <30929839+VillSource@users.noreply.github.com>
2026-09-13 08:15:49 +0700

    remove template

--- src/VillsourceModules/Organization - Use To copy for new module/Villsource.FSH.Modules.Organization.Contracts/OrganizationContractsMarker.cs
+++
@@ -1,7 +0,0 @@
-namespace Villsource.FSH.Modules.Organization.Contracts;
-
-/// <summary>
-/// Marker referenced by <c>Program.cs::AddMediator(o => o.Assemblies = [...])</c> so the
-/// Mediator source generator scans this assembly for ICommand/IQuery records.
-/// </summary>
-public abstract partial class OrganizationContractsMarker;
--- src/VillsourceModules/Organization - Use To copy for new module/Villsource.FSH.Modules.Organization/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Runtime.CompilerServices;
-using FSH.Framework.Web.Modules;
-using Villsource.FSH.Modules.Organization;
-
-[assembly: FshModule(typeof(OrganizationModule))]
-[assembly: InternalsVisibleTo("OrganizationModule.Tests")]
-[assembly: InternalsVisibleTo("OrganizationModule.Tests")]
